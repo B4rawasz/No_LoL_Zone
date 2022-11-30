2093fe649124782f0144217252d0810b0aba2056
B4rawasz <69699459+B4rawasz@users.noreply.github.com>
2022-11-30 16:47:49 +0100

    No LoL Zone
    First upload

---
+++ Stay safe/Stay safe/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Stay_safe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stay safe!!!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Stay_safe.Properties.Resources._2057530;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 239);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "With me, you are safe";
+            this.notifyIcon.BalloonTipTitle = "I\'m here";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "No LoL zone\r\nYou are safe bro <3";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(36)))), ((int)(((byte)(103)))));
+            this.ClientSize = new System.Drawing.Size(223, 350);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(239, 389);
+            this.MinimumSize = new System.Drawing.Size(239, 389);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "No LoL zone";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private NotifyIcon notifyIcon;
+    }
+}
---
+++ Stay safe/Stay safe/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Stay_safe
+{
+    public partial class Form1 : Form
+    {
+        Thread app = new Thread(LookFor.lookForApp);
+
+        //NotifyIcon notifyIcon = new NotifyIcon();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            app.Start();
+            icon = notifyIcon;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LookFor.run = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+                notifyIcon.ShowBalloonTip(300);
+            }
+        }
+
+        public static NotifyIcon icon;
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        public static void rescue()
+        {
+            icon.BalloonTipText = "That was close 😅";
+            icon.BalloonTipTitle = "BRO!";
+            icon.ShowBalloonTip(300);
+            icon.BalloonTipText = "With me, you are safe";
+            icon.BalloonTipTitle = "I'm here";
+        }
+    }
+
+    public class LookFor
+    {
+        public static bool run = true;
+        public static void lookForApp()
+        {
+            while (run)
+            {
+                Thread.Sleep(10000);
+                Process[] runingProcess = Process.GetProcesses();
+
+                for (int i = 0; i < runingProcess.Length; i++)
+                {
+                    if (runingProcess[i].ProcessName == "LeagueClient")
+                    {
+                        runingProcess[i].Kill();
+                        Form1.rescue();
+                    }
+                }
+            }
+        }
+    }
+}
